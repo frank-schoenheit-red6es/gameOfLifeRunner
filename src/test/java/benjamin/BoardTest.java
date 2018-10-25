36eeb60e725975752acf3229f7e8aab478e4a09f
Frank Schönheit <frank.schoenheit@red6-es.de>
2018-10-25 16:07:32 +0200

    BoardTest

---
+++ src/test/java/benjamin/BoardTest.java
@@ -0,0 +1,15 @@
+package benjamin;
+
+import de.red6.gameoflife.board.AbstractBoardTest;
+import static org.junit.Assert.*;
+import org.junit.Ignore;
+
+/**
+ * @author <a href="mailto:frank.schoenheit@red6-es.de">Frank Scho&ouml;nheit</a>
+ */
+@Ignore
+public class BoardTest extends AbstractBoardTest {
+    public BoardTest() {
+        super(Board.class);
+    }
+}
